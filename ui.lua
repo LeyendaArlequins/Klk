8a4d3898afcda71f5f852dc479412da460405b66
LeyendaArlequins <161185259+LeyendaArlequins@users.noreply.github.com>
2025-12-01 22:29:49 -0600

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -126,7 +126,7 @@ function Library:Window(title)
     Logo.BackgroundTransparency = 1.000
     Logo.Position = UDim2.new(0, 4, 0.5, 0)
     Logo.Size = UDim2.new(0, 26, 0, 30)
-    Logo.Image = "http://www.roblox.com/asset/?id=110728705873113"
+    Logo.Image = "http://www.roblox.com/asset/?id=99176549590456"
     Logo.ImageColor3 = Color3.fromRGB(232, 17, 85)
     
     -- Minimize Button (using minus icon)
@@ -138,7 +138,7 @@ function Library:Window(title)
     Minimize.BackgroundTransparency = 1.000
     Minimize.Position = UDim2.new(1, -6, 0.5, 0)  -- ← DE -30 A -6
     Minimize.Size = UDim2.new(0, 20, 0, 20)
-    Minimize.Image = "rbxassetid://7733771811" -- Minus icon
+    Minimize.Image = "rbxassetid://0" -- Minus icon
     Minimize.ImageColor3 = Color3.fromRGB(199, 199, 199)
     Minimize.ScaleType = Enum.ScaleType.Crop
         
@@ -154,7 +154,8 @@ function Library:Window(title)
     MinimizedIcon.Size = UDim2.new(0, 40, 0, 40)
     MinimizedIcon.Visible = false
     MinimizedIcon.ZIndex = 10
-    MinimizedIcon.Image = "http://www.roblox.com/asset/?id=110728705873113"
+    MinimizedIcon.Image = "http://www.roblox.com/asset/?id=
+99176549590456"
     
     local MinimizedCorner = Instance.new("UICorner")
     MinimizedCorner.CornerRadius = UDim.new(0, 8)
